90a28459f9a5b6f4858b55f531cc5052e5e5aaea
juhuashengkaidewanshang <642354045@qq.com>
2014-02-14 15:01:17 +0800

    tijiao 7

--- client/app/controllers/quotaIntergrityControllerr.js
+++ client/app/controllers/quotaIntergrityControllerr.js
@@ -8,8 +8,7 @@
 
 App.QuotaIntegrityController = Ember.ObjectController.extend({
 	selectedEntityEnum : {},
-  items: Em.A(),
-	
+	items : Em.A(),
 
 	selectedEntity : function() {
 		this.refresh();
@@ -20,37 +19,27 @@ App.QuotaIntegrityController = Ember.ObjectController.extend({
 		var self = this;
 		var entityID = self.get('selectedEntityEnum.key');
 		console.log(entityID);
-    //todo clear items
-    self.items.clear();
-    
-    App.Quota.findFieldByResType(entityID).then(function(data) {
+		//todo clear items
+		self.items.clear();
+
+
+		App.Quota.findFieldByResType(entityID).then(function(data) {
 			data.forEach(function(field) {
 				var item = new App.CheckObject();
-				
-				
-				item.set('evaluateItemID',field.evaluateItemID);
-				item.set('propertyName',field.propertyName);
-				item.set('propertyID',field.propertyID);
-				item.set('typeName',field.typeName);
-				item.set('typeID',field.typeID);
-				if(field.evaluateItemID==null ){
-					item.set('ischeck',false);
-				}else{
-					item.set('ischeck',true);
+
+				item.set('evaluateItemID', field.evaluateItemID);
+				item.set('propertyName', field.propertyName);
+				item.set('propertyID', field.propertyID);
+				item.set('typeName', field.typeName);
+				item.set('typeID', field.typeID);
+				if (field.evaluateItemID == null) {
+					item.set('ischeck', false);
+				} else {
+					item.set('ischeck', true);
 				}
 				item.set('isLoading', false);
-				self.items.pushObject(item);	
+				self.items.pushObject(item);
 			})
 		});
-    
-  //  for (i = 0; i < self.data.items.length; i++) {
-   //   var _item =  self.data.items[i];
-   //   var item = new App.CheckObject();
-    //  item.set('id', _item.id);
-    //  item.set('name', _item.name);
-    //  item.set('type', _item.type);
-    //  item.set('ischeck', _item.ischeck);
-    //  item.set('isLoading', false);
-    //  self.items.pushObject(item);
-    }
+	}
 });
